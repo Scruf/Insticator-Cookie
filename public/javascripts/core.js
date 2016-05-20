60bdc11618f9c7ab07e1411dbc4a8702d05d3e17
Egor <egor.kozitski@mail.citytech.cuny.edu>
2016-05-20 19:52:50 -0400

    Added images for cookies

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -1,4 +1,9 @@
 var cookies = angular.module('cookies',[]);
 function cookie_view($scope,$http){
-    
+    $scope.formData= [];
+    $http.get('/api/cookies')
+    .success(function(data){
+    	$scope.cookies = data;
+    	console.log(data);
+    });
 }
